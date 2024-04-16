e2f969082dbbd02dc1e6ac82f8c8c5f7494c2845
Zephyls <131590154+Zephyls@users.noreply.github.com>
2024-04-16 15:01:12 +0200

    Add files via upload

--- Nemweb/Restaurantapp/urls.py
+++ Nemweb/Restaurantapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views 
 urlpatterns = [
-    path("", views.home, name="home")
+    path('', views.home, name='home'),
+    path('s', views.aboutus, name='aboutus'),
+    path('', views.contact, name='contact'),
 
 ]
--- Nemweb/Restaurantapp/views.py
+++ Nemweb/Restaurantapp/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def home(request):
     return render(request,"home.html")
+def aboutus(request):
+    return render(request,"aboutus.html");
+def contact(request):
+    return render(request,"contact.html");
+def cart(request):
+    return render(request,"cart.html");
